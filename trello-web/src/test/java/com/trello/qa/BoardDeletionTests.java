691c2477da7d746dad440bf105f86ccefe1e052f
natali62040 <54206199+natali62040@users.noreply.github.com>
2019-09-12 14:53:18 +0300

    BoardTests is created

--- trello-web/src/test/java/com/trello/qa/BoardDeletionTests.java
+++ trello-web/src/test/java/com/trello/qa/BoardDeletionTests.java
@@ -21,29 +21,5 @@ public class BoardDeletionTests extends TestBase {
         Assert.assertEquals(after, before-1);
     }
 
-    public void clickOnDeleteButton() {
-        click(By.xpath("//*[@class='js-confirm full negate']"));
-    }
-
-    public void clickOnPermanentlyDeleteBoard() {
-        click(By.xpath("//*[@class='quiet js-delete']"));
-    }
-
-    public void clickOnCloseBoardButton() {
-        click(By.xpath("//*[@class='js-confirm full negate']"));
-    }
-
-    public void clickOnCloseBoard() {
-        click(By.xpath("//*[@class='board-menu-navigation-item-link js-close-board']"));
-    }
-
-
-    public void clickOnMore() {
-        click(By.xpath("//*[@class ='board-menu-navigation-item-link js-open-more']"));
-    }
-
-    public void clickOnFirstBoard() {
-        click(By.xpath("//h3[contains(text(),'Personal Boards')]/../..//li[1]"));
-    }
 
 }
--- trello-web/src/test/java/com/trello/qa/TestBase.java
+++ trello-web/src/test/java/com/trello/qa/TestBase.java
@@ -149,4 +149,28 @@ public class TestBase {
     public boolean isTherePersonalBoards() {
         return isElementPresent(By.xpath("//*[@class='icon-lg icon-member']/../../.."));
     }
+
+    public void clickOnDeleteButton() {
+        click(By.xpath("//*[@class='js-confirm full negate']"));
+    }
+
+    public void clickOnPermanentlyDeleteBoard() {
+        click(By.xpath("//*[@class='quiet js-delete']"));
+    }
+
+    public void clickOnCloseBoardButton() {
+        click(By.xpath("//*[@class='js-confirm full negate']"));
+    }
+
+    public void clickOnCloseBoard() {
+        click(By.xpath("//*[@class='board-menu-navigation-item-link js-close-board']"));
+    }
+
+    public void clickOnMore() {
+        click(By.xpath("//*[@class ='board-menu-navigation-item-link js-open-more']"));
+    }
+
+    public void clickOnFirstBoard() {
+        click(By.xpath("//h3[contains(text(),'Personal Boards')]/../..//li[1]"));
+    }
 }
